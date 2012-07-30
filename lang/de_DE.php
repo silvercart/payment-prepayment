ecf76530602d1e038a3145972e6a7aa31ac886e6
Roland Lehmann <rlehmann@pixeltricks.de>
2012-07-30 21:38:34 +0200

    MINOR:  added call of the global $lang in translation files;  global $lang called after file include

--- lang/de_DE.php
+++ lang/de_DE.php
@@ -26,6 +26,8 @@
 
 i18n::include_locale_file('silvercart', 'en_US');
 
+global $lang;
+
 if (array_key_exists('de_DE', $lang) && is_array($lang['de_DE'])) {
     $lang['de_DE'] = array_merge($lang['en_US'], $lang['de_DE']);
 } else {
