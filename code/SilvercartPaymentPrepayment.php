7978a63384ab62e01ce6ff0222f954e37c4d9c94
Roland Lehmann <rlehmann@pixeltricks.de>
2013-02-15 08:09:18 +0100

    DEVELOPMENT renamed TabSet of CMS fields from Sections to Root

--- code/SilvercartPaymentPrepayment.php
+++ code/SilvercartPaymentPrepayment.php
@@ -188,12 +188,12 @@ class SilvercartPaymentPrepayment extends SilvercartPaymentMethod {
         // Add fields to default tab ------------------------------------------
         $channelField = new ReadonlyField('DisplayPaymentChannel', _t('SilvercartPaymentPrepayment.PAYMENT_CHANNEL'), $this->getPaymentChannelName($this->PaymentChannel));
 
-        $fields->addFieldToTab('Sections.Basic', $channelField, 'mode');
+        $fields->addFieldToTab('Root.Basic', $channelField, 'mode');
         
         // Additional tabs and fields -----------------------------------------
         $tabTextTemplates = new Tab(_t('SilvercartPaymentPrepayment.TEXT_TEMPLATES', 'text templates', null, 'Textvorlagen'));
-        #$fields->addFieldToTab('Sections.Translations', new ComplexTableField($this, 'SilvercartPaymentPrepaymentLanguages', 'SilvercartPaymentPrepaymentLanguage'));
-        $fields->fieldByName('Sections')->push($tabTextTemplates);
+        #$fields->addFieldToTab('Root.Translations', new ComplexTableField($this, 'SilvercartPaymentPrepaymentLanguages', 'SilvercartPaymentPrepaymentLanguage'));
+        $fields->fieldByName('Root')->push($tabTextTemplates);
         // text templates for tab fields
         // Textvorlagen Tab Felder --------------------------------------------
         $languageFields = SilvercartLanguageHelper::prepareCMSFields($this->getLanguageClassName());
